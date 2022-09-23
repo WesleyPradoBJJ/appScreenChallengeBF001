c5c9229d472a46feaac27160776f3750354fb90e
Wesley Prado <wesleypradobjj@gmail.com>
2022-09-23 13:06:45 +0400

    Second Commit / Logic and visual requirements implemented.

--- appScreenChallengeBF001/appScreenChallengeBF001/ViewController.swift
+++ appScreenChallengeBF001/appScreenChallengeBF001/ViewController.swift
@@ -26,10 +26,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 242/255, green: 188/255, blue: 12/255, alpha: 1.0)// Main view background's color. 
         configLabel()
         configTextField()
         configButton()
+        registerButton.isEnabled = false
     }
+    
     
     func configLabel(){
         titleLabel.text = "AppScreenChallenge"
@@ -40,41 +43,100 @@ class ViewController: UIViewController, UITextFieldDelegate {
         subtitleLabel.text = "Tela de Cadastro"
         subtitleLabel.textAlignment = .center
         subtitleLabel.font = .boldSystemFont(ofSize: 18)
-        subtitleLabel.textColor = .darkGray
+        subtitleLabel.textColor = .black
         
         nameLabel.text = "Nome:"
         nameLabel.textAlignment = .left
         nameLabel.font = .boldSystemFont(ofSize: 15)
-        nameLabel.textColor = .darkGray
+        nameLabel.textColor = .black
         
         adressLabel.text = "Endereço:"
         adressLabel.textAlignment = .left
         adressLabel.font = .boldSystemFont(ofSize: 15)
-        adressLabel.textColor = .darkGray
+        adressLabel.textColor = .black
         
         passwordLabel.text = "Senha:"
         passwordLabel.textAlignment = .left
         passwordLabel.font = .boldSystemFont(ofSize: 15)
-        passwordLabel.textColor = .darkGray
+        passwordLabel.textColor = .black
     }
+    
     
     func configTextField(){
         nameTextField.placeholder = "Por favor, digite seu nome."
+        nameTextField.textColor = UIColor.black
+        nameTextField.backgroundColor = UIColor(red: 255/255, green: 237/255, blue: 165/255, alpha: 1.0)
         nameTextField.delegate = self
         
         adressTextField.placeholder = "Por favor, digite seu endereço."
+        adressTextField.textColor = UIColor.black
+        adressTextField.backgroundColor = UIColor(red: 255/255, green: 237/255, blue: 165/255, alpha: 1.0)
         adressTextField.delegate = self
         
         passwordTextField.placeholder = "Por favor, digite sua senha."
+        passwordTextField.textColor = UIColor.black
+        passwordTextField.backgroundColor = UIColor(red: 255/255, green: 237/255, blue: 165/255, alpha: 1.0)
         passwordTextField.delegate = self
     }
+    
     
     func configButton (){
         registerButton.setTitle("Cadastrar", for: .normal)
         registerButton.tintColor = .darkGray
+        registerButton.backgroundColor = UIColor(red: 255/255, green: 237/255, blue: 165/255, alpha: 1.0)
         registerButton.layer.borderWidth = 2
         registerButton.layer.cornerRadius = 15
     }
 
+    //Does the text field border color should immediately turn blue  when the keyboard rises?
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("TextFieldDidBeginEditing-ok")
+        //textField.layer.borderColor = UIColor.blue.cgColor
+        switch textField {
+        case nameTextField:
+             nameTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        case adressTextField:
+            adressTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        case passwordTextField:
+            passwordTextField.layer.borderColor = UIColor.blue.cgColor
+            
+        default:
+            break
+        }
+        
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("TextFieldDidEndEditing-ok")
+        if textField.text == ""{
+            textField.layer.borderWidth = 2
+            textField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            textField.layer.borderWidth = 0
+        }
+        
+        //Validação do botão
+        if nameTextField.text == "" || adressTextField.text == "" || passwordTextField.text == ""{
+                   registerButton.isEnabled = false
+               } else {
+                   registerButton.isEnabled = true
+               }
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("TextFieldShouldReturn-OK")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+        print("Cadastro realizado com sucesso!")
+    }
 }
 
